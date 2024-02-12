780617a03c2038ad6195c1549d7b57edc38359b8
barre <89933964+xXBarre@users.noreply.github.com>
2024-02-12 09:55:51 +0100

    fotos + prueba

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel");
+    let currentIndex = 0;
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % (carousel.children.length);
+        updateCarousel();
+    }
+
+    function updateCarousel() {
+        const translateValue = -currentIndex * 100 + "%";
+        carousel.style.transform = "translateX(" + translateValue + ")";
+    }
+
+    setInterval(nextSlide, 3000); // Cambiar de slide cada 3 segundos (ajustar según sea necesario)
+});
