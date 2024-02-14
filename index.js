a51c33418c57dec808b2a8d7dbac3e8a8fc84b15
barre <89933964+xXBarre@users.noreply.github.com>
2024-02-14 22:10:08 +0100

    CAmbie nombre al archivo inicioA.html a index.html + añadi inicio de un boton de subida (no funciona aun)

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /*Nico*/
+/*Boton top*/
+
+const sub = document.getElementById("top")
+sub.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
+
 /*Cesta*/
 
 
@@ -6,22 +16,6 @@
 
 
 /*Alba*/
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".carousel");
-    let currentIndex = 0;
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % (carousel.children.length);
-        updateCarousel();
-    }
-
-    function updateCarousel() {
-        const translateValue = -currentIndex * 100 + "%";
-        carousel.style.transform = "translateX(" + translateValue + ")";
-    }
-
-    setInterval(nextSlide, 3000); // Cambiar de slide cada 3 segundos (ajustar según sea necesario)
-});
 
 /*Laura*/
 document.addEventListener("DOMContentLoaded", function() {
