22c855ca23429b3c215ee4f95cf762d13d35792b
barre <89933964+xXBarre@users.noreply.github.com>
2024-02-15 17:42:55 +0100

    Inicio de nueva cesta, la otra no me funcina

--- index.js
+++ index.js
@@ -10,224 +10,169 @@ sub.addEventListener('click', () => {
   })
 })
 
-/*Cesta*/
+/*Carrito*/
+// Array para almacenar los productos en el carrito
+let carrito = [];
+let imgCarro = document.getElementById('carrito')
 
-//funcion para almacenar los datos para poder usarlos
-function guardarAlmacenamientoLocal(llave, valor_a_guardar){
-  localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
-}
-function obtenerAlmacenamientoLocal(llave){
-  const datos = JSON.parse(localStorage.getItem(llave))
-  return datos
-}
-let productos = obtenerAlmacenamientoLocal('productos') || [];
-
-//variables del html
-const informacionCompra = document.getElementById('informacionCompra');
-const contenedorCompra = document.getElementById('contenedorCompra');
-const productosCompra = document.getElementById('productosCompra');
-const carrito = document.getElementById('carrito');
-const numero = document.getElementById("numero");
-const total = document.getElementById('total');
-const x = document.getElementById('x')
-const header = document.querySelector("#header");
-const contenedor = document.querySelector("#contenedor");
-const body = document.querySelector("body");
-
-let lista = []
-let valortotal = 0
-
-window.onload = function (){
-  visualizarProductos();
-  contenedorCompra.classList.add("none")
-} 
-
-function visualizarProductos() {
-  contenedor.innerHTML = ""
-  for (let i = 0; i < productos.length; i++) {
-    if (productos[i].existencia > 0) {
-      contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">${productos[i].valor}€</p><button onclick=comprar(${i})>Comprar</button></div></div>`
-    }
-      else {
-      contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">${productos[i].valor}€</p><p class="soldOut">Sold Out</p></div></div>`
-    }
-  }
+// Función para agregar un producto al carrito
+function agregarAlCarrito(nombre, precio) {
+    const producto = { nombre, precio };
+    carrito.push(producto);
+    mostrarCarrito();
 }
 
-function comprar(indice)
-{
-  lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
+// Función para mostrar los productos en el carrito
+function mostrarCarrito() {
+    const productosContainer = document.getElementById('productos');
+    productosContainer.innerHTML = '';
 
-  let van = true
-  let i = 0
-  while (van == true)
-  {
-    if (productos[i].nombre == productos[indice].nombre) 
-    {
-      productos[i].existencia -= 1
-      if (productos[i].existencia == 0)
-      {
-        visualizarProductos()
-      }
-      van = false
-    }
-    guardarAlmacenamientoLocal("productos", productos)
-        i += 1
-  }
-  numero.innerHTML = lista.length
-  numero.classList.add("diseñoNumero")
-  return lista
+    carrito.forEach(producto => {
+        const productoElement = document.createElement('div');
+        productoElement.classList.add('producto');
+        productoElement.innerHTML = `<span>${producto.nombre} - ${producto.precio} €</span>`;
+        productosContainer.appendChild(productoElement);
+    });
 }
 
-carrito.addEventListener("click", function(){
-    body.style.overflow = "hidden"
-    contenedorCompra.classList.remove('none')
-    contenedorCompra.classList.add('contenedorCompra')
-    informacionCompra.classList.add('informacionCompra')
-    mostrarElemtrosLista()
-})
-
-function mostrarElemtrosLista() {
-    productosCompra.innerHTML = ""
-    valortotal = 0
-    for (let i = 0; i < lista.length; i++){
-        productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> ${lista[i].precio}€</p></div>`
-        valortotal += parseInt(lista[i].precio)
-    }
-    total.innerHTML = `<p>Valor Total</p> <p><span>${valortotal}€</span></p>`
+//Comprar
+function comprar() {
+    alert('Esta funcion aun no esta disponible por el momento, espera a que hagamos la practica de php.')
+    carrito = [];
+    location.reload();
 }
 
-function eliminar(indice){
-    let van = true
-    let i = 0
-    while (van == true) {
-        if (productos[i].nombre == lista[indice].nombre) {
-            productos[i].existencia += 1
-            lista.splice(indice, 1)
-            van = false
-        }
-        i += 1
-    }
-    guardarAlmacenamientoLocal("productos", productos)
-
-    numero.innerHTML = lista.length
-    if (lista.length == 0){
-        numero.classList.remove("diseñoNumero")
-    }
-    visualizarProductos()
-    mostrarElemtrosLista()
+// Función para vaciar el carrito
+function vaciarCarrito() {
+    carrito = [];
+    mostrarCarrito();
 }
 
-x.addEventListener("click", function(){
-    body.style.overflow = "auto"
-    contenedorCompra.classList.add('none')
-    contenedorCompra.classList.remove('contenedorCompra')
-    informacionCompra.classList.remove('informacionCompra')
-})
+// Asociar eventos a los botones de compra
+document.querySelectorAll('.bcomprar').forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const nombreProducto = document.querySelectorAll('.informacion h2')[index].textContent.trim();
+        const precioProducto = document.querySelectorAll('.informacion .precio')[index].textContent.trim();
+        const precioNumerico = parseFloat(precioProducto.replace('€', '').replace(',', '.'));
+        agregarAlCarrito(nombreProducto, precioNumerico);
+    });
+});
+
+// Asociar evento al botón de vaciar carrito
+document.getElementById('botonCarro').addEventListener('click', vaciarCarrito);
+
+//Comprar
+document.getElementById('botonCorro').addEventListener('click', comprar);
+
+
+
+
+
+
 
 
 /*Hugo*/
 
-window.onload = function(){
-  let signUp = document.getElementById("signUp");
-  let signIn = document.getElementById("signIn");
-  let nameInput = document.getElementById("nameInput");
-  let confirmPasswordInput = document.getElementById("confirmPasswordInput");
-  let title = document.getElementById("title");
+// window.onload = function(){
+//   let signUp = document.getElementById("signUp");
+//   let signIn = document.getElementById("signIn");
+//   let nameInput = document.getElementById("nameInput");
+//   let confirmPasswordInput = document.getElementById("confirmPasswordInput");
+//   let title = document.getElementById("title");
 
-  signIn.onclick = function() {
-  nameInput.style.maxHeight = "0";
-  confirmPasswordInput.style.maxHeight = "0";
-  title.innerHTML = "Login";
-  signUp.classList.add("disable");
-  signIn.classList.remove("disable");
-  }
+//   signIn.onclick = function() {
+//   nameInput.style.maxHeight = "0";
+//   confirmPasswordInput.style.maxHeight = "0";
+//   title.innerHTML = "Login";
+//   signUp.classList.add("disable");
+//   signIn.classList.remove("disable");
+//   }
 
-  signUp.onclick = function() {
-  nameInput.style.maxHeight = "60px";
-  confirmPasswordInput.style.maxHeight = "60px";
-  title.innerHTML = "Registro";
-  signUp.classList.remove("disable");
-  signIn.classList.add("disable");
-  }
-}
+//   signUp.onclick = function() {
+//   nameInput.style.maxHeight = "60px";
+//   confirmPasswordInput.style.maxHeight = "60px";
+//   title.innerHTML = "Registro";
+//   signUp.classList.remove("disable");
+//   signIn.classList.add("disable");
+//   }
+// }
 
   
     
-/*
-function validarFormulario() {
-        // Obtener los valores del formulario
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
+// /*
+// function validarFormulario() {
+//         // Obtener los valores del formulario
+//         var password = document.getElementById("password").value;
+//         var confirmPassword = document.getElementById("confirmPassword").value;
 
-                // Verificar si la contraseña coincide con la confirmación de contraseña solo en la zona de registro
-                if (password !== confirmPassword) {
-                    alert("La contraseña y la confirmación de contraseña no coinciden, por favor inténtelo de nuevo");
-                    return;
-                }
+//                 // Verificar si la contraseña coincide con la confirmación de contraseña solo en la zona de registro
+//                 if (password !== confirmPassword) {
+//                     alert("La contraseña y la confirmación de contraseña no coinciden, por favor inténtelo de nuevo");
+//                     return;
+//                 }
 
-                // Verificar si la contraseña contiene símbolos, números y letras
-                var passwordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/;
-                if (!passwordPattern.test(password)) {
-                    alert("La contraseña debe contener al menos una letra, un número y un símbolo");
-                    return;
-                }
+//                 // Verificar si la contraseña contiene símbolos, números y letras
+//                 var passwordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/;
+//                 if (!passwordPattern.test(password)) {
+//                     alert("La contraseña debe contener al menos una letra, un número y un símbolo");
+//                     return;
+//                 }
 
-                // Resto del código para enviar el formulario o realizar otras acciones
-                alert("Formulario válido. Puedes realizar la acción deseada.");
-            }
-*/
+//                 // Resto del código para enviar el formulario o realizar otras acciones
+//                 alert("Formulario válido. Puedes realizar la acción deseada.");
+//             }
+// */
 
-//Laura
+// //Laura
 
-window.onload = function iniciarAnimacion() 
-{
-  const numColumnas = 10;
-  const distanciaColumnas = window.innerWidth / numColumnas;
-  const columnas = Array.from({ length: numColumnas }, (_, i) => i);
+// window.onload = function iniciarAnimacion() 
+// {
+//   const numColumnas = 10;
+//   const distanciaColumnas = window.innerWidth / numColumnas;
+//   const columnas = Array.from({ length: numColumnas }, (_, i) => i);
 
-  setInterval(() => {
-    const columna = columnas.shift();
-    columnas.push(columna);
+//   setInterval(() => {
+//     const columna = columnas.shift();
+//     columnas.push(columna);
 
-    const flor = document.createElement('div');
-    flor.className = 'contenedor-flor';
-    flor.style.left = columna * distanciaColumnas + 'px';
-    flor.style.animationDuration = Math.random() * 2 + 1 + 's';
-    document.getElementById('lienzo').appendChild(flor);
+//     const flor = document.createElement('div');
+//     flor.className = 'contenedor-flor';
+//     flor.style.left = columna * distanciaColumnas + 'px';
+//     flor.style.animationDuration = Math.random() * 2 + 1 + 's';
+//     document.getElementById('lienzo').appendChild(flor);
                 
-    setTimeout(() => {
-      flor.remove();
-    }, 3000);
-  }, 500);
-}
+//     setTimeout(() => {
+//       flor.remove();
+//     }, 3000);
+//   }, 500);
+// }
 
-/*Alba*/
+// /*Alba*/
 
-let curIndex = 0;
+// let curIndex = 0;
 
-function showLodedentro(index) {
-    const lodedentro = document.querySelector('.lodedentros');
-    const totalLodedentros = document.querySelectorAll('.lodedentro').length;
+// function showLodedentro(index) {
+//     const lodedentro = document.querySelector('.lodedentros');
+//     const totalLodedentros = document.querySelectorAll('.lodedentro').length;
 
-    if (index >= totalLodedentros) {
-        curIndex = 0;
-    } else if (index < 0) {
-        curIndex = totalLodedentros - 1;
-    } else {
-        curIndex = index;
-    }
+//     if (index >= totalLodedentros) {
+//         curIndex = 0;
+//     } else if (index < 0) {
+//         curIndex = totalLodedentros - 1;
+//     } else {
+//         curIndex = index;
+//     }
 
-    const translateValue = -curIndex * 100 + '%';
-    lodedentro.style.transform = `translateX(${translateValue})`;
-}
+//     const translateValue = -curIndex * 100 + '%';
+//     lodedentro.style.transform = `translateX(${translateValue})`;
+// }
 
-function prevLodedentro() {
-    showLodedentro(curIndex - 1);
-}
+// function prevLodedentro() {
+//     showLodedentro(curIndex - 1);
+// }
 
-function nextLodedentro() {
-    showLodedentro(curIndex + 1);
-}
+// function nextLodedentro() {
+//     showLodedentro(curIndex + 1);
+// }
 
-setInterval(nextLodedentro, 3000);
+// setInterval(nextLodedentro, 3000);
