bdbcab6e0caee8dc84984c5c97941fff5cde6bb0
barre <89933964+xXBarre@users.noreply.github.com>
2024-03-07 22:56:42 +0100

    Falta este archivo

---
+++ head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script defer src="index.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+
